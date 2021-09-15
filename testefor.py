ea7b4182995a3db3f249dc02e5537f67b3efd757
Susana S Silva <susana_santos@id.uff.br>
2021-09-15 13:50:45 -0300

    Exercícios UriOnlineJudge

--- testefor.py
+++
@@ -1,9 +0,0 @@
-for item in [3,4,5,6,7,8,9]:
-    print(item, end=" ")
-print()
-
-num = int(input("Digite valor inteiro e positivo:"))
-fat = 1
-for i in range(1,num+1):
-    fat = fat*i
-print("O fatorial de", num, "=", fat)
